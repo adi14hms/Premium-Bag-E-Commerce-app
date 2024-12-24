02b99c9ea031dc3d1aa79c7eb6a6406e0cb8ba4e
Aditya jha <adi14hms@gmail.com>
2024-12-25 01:53:16 +0530

    added create owner route and mongodb connection and tested on postman

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const ownerModel = require('../models/owner-model');
 const router = express.Router();
+
+if(process.env.NODE_ENV==="development"){
+    router.post("/create", async function(req,res){
+    let owners = await ownerModel.find();
+    if(owners.length>0){
+        return res
+        .status(503)
+        .send("you don't have permission to create")
+    }
+    let {fullname, email,password}=req.body;
+    let createdOwner = await ownerModel.create({
+        fullname,
+        email,
+        password,
+    })
+    res.status(201).send(createdOwner);
+});
+}
 
 
 router.get("/",function(req,res){
     res.send("hey its working");
-})
+});
+
+
 
 module.exports =router;
